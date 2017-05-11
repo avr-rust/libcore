a1af51c0bf72080d9f9fdfaac4887a2464b9e988
Dr. ERDI Gergo <gergo@erdi.hu>
2017-05-11 21:27:44 +0800

    Avoid creating superfluous static strings for panic messages

--- src/macros.rs
+++ src/macros.rs
@@ -17,15 +17,15 @@ macro_rules! panic {
         panic!("explicit panic")
     );
     ($msg:expr) => ({
-        static _MSG_FILE_LINE: (&'static str, &'static str, u32) = ($msg, file!(), line!());
-        $crate::panicking::panic(&_MSG_FILE_LINE)
+        static _MSG_FILE_LINE: (&'static str, &'static str, u32) = ("", "", 0);
+        $crate::panicking::panic(&_MSG_FILE_LINE);
     });
     ($fmt:expr, $($arg:tt)*) => ({
         // The leading _'s are to avoid dead code warnings if this is
         // used inside a dead function. Just `#[allow(dead_code)]` is
         // insufficient, since the user may have
         // `#[forbid(dead_code)]` and which cannot be overridden.
-        static _FILE_LINE: (&'static str, u32) = (file!(), line!());
+        static _FILE_LINE: (&'static str, u32) = ("", 0);
         // $crate::panicking::panic_fmt(format_args!($fmt, $($arg)*), &_FILE_LINE)
         loop {}
     });
